c856ee265de647a01f8b4bb1b7e745a41f8b23c9
John Smalley <john.m.smalley@gmail.com>
2016-07-21 21:26:28 -0500

    "first commit"

---
+++ TechDryRun.js
@@ -0,0 +1,54 @@
+//Tech Dry Run
+
+var hrr18 = {
+  name: 'Keeley Nakamoto',
+  anothername: 'Jim',
+  jeffName: 'Jeff Bernstein',
+  yetAnother: 'Dan jrrn eerr n nnrr rner rnnr  eees n oynllolonyro ynronllooo loo
+
+ ro
+nnr
+
+ name: 'Caleb',
+
+  neekon: neekon,
+  per
+
+so
+nskf;lasdkf;lasdkf
+
+  na
+myyyydddd
+
+//John Test hi
+
+//Jason Here! good to go.
+
+ss
+
+yy
+PayPe
+kdkdkdkdkdk hello
+
+// STEPHENIE CHANGES -- //
+
+y: 'PPayt:yy}:::
+:
+yylo
+ama
+eeeaeema:meeaamaeaaeeaeaaeeeeeeee:e::eee emmea::a esnY
+
+hello
+Hello
+
+
+
+sa
+name: 'Mchael Olorunnisola'
+}
+
+
+//Yara
+
+// HOON IS HERE FINALLY
+var test = Hoon;
